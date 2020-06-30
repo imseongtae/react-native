c5eabf5891a28052f4e483ab1c77f6665fe4b043
imseongtae <dev.haemil@gmail.com>
2020-07-01 00:47:06 +0900

    feat: Set up absolute path

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,14 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
   // 디버깅시 class명을 확인하기 쉽게 만들어 줍
-  plugins: ['babel-plugin-styled-components'],
+  plugins: [
+    'babel-plugin-styled-components',
+    [
+      'babel-plugin-root-import',
+      {
+        rootPathPrefix: '~',
+        rootPathSuffix: 'src',
+      },
+    ],
+  ],
 };
